8826f6da67dbd8d1a25efed48ed4b28c2721bd25
Richard (Rick) Hawkins <hurricanerix@gmail.com>
2014-02-22 20:36:38 -0600

    Created SpriteManager class.

--- transylvania/__init__.py
+++ transylvania/__init__.py
@@ -36,7 +36,7 @@ class Application(object):
     Application class for the Transylvania Engine.
     """
 
-    def __init__(self, config=None, display=None):
+    def __init__(self, config=None, display=None, sprite_manager=None):
         """
         Initialize the application.
 
@@ -47,6 +47,7 @@ class Application(object):
         """
         self.config = config
         self.display = display
+        self.sprite_manager = sprite_manager
         self.running = True
         self.objects = []
 
@@ -67,7 +68,16 @@ class Application(object):
         """
         Start the application event loop.
         """
+        self.display.init_window()
+
+        self.sprite_manager.load('buddah', 'wall_face')
+        face = self.sprite_manager.get_sprite('wall_face')
+        buddah = self.sprite_manager.get_sprite('buddah')
+
         event = SDL_Event()
+
+        light_position = [0.0, 250.0, -10.0]
+        light_dir = 1.0
 
         while self.running:
             while SDL_PollEvent(ctypes.byref(event)) != 0:
@@ -76,11 +86,26 @@ class Application(object):
                 if event.type == events.SDL_KEYDOWN:
                     self.handle_input(event.key.keysym.sym)
 
-            self.display.start_render()
-            self.objects.sort(key=lambda obj: obj.layer)
-            proj_mat = self.display.get_proj_matrix()
+            if light_position[0] > 600.0:
+                light_position[0] = 600.0
+                light_dir = -1
+            if light_position[0] < -2.0:
+                light_position[0] = -2.0
+                light_dir = 1.0
+            light_position[0] = light_position[0] + light_dir
 
-            for obj in self.objects:
-                obj.draw(proj_mat)
+            self.display.start_render()
+            #self.objects.sort(key=lambda obj: obj.layer)
+            proj_matrix = self.display.get_proj_matrix()
+
+            for x in xrange(int(self.display.width / 150) + 1):
+                for y in xrange(int(self.display.height / 150) + 1):
+                    face.draw(proj_matrix, x * 150, y * 150, layer=0,
+                              light_position=light_position)
+            buddah.draw(proj_matrix, light_position[0], 250, layer=1,
+                        light_position=light_position)
+
+            #for obj in self.objects:
+            #    obj.draw(proj_mat)
 
             self.display.stop_render()
